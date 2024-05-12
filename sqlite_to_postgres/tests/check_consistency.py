5d804c6a74b8b17f49c3cbedf48d516ee5cbc20d
nafedoseev <nafedoseev@domclick.ru>
2024-05-12 22:56:49 +0300

    правки после ревью

--- sqlite_to_postgres/tests/check_consistency.py
+++ sqlite_to_postgres/tests/check_consistency.py
@@ -11,7 +11,7 @@ pg_dsl = {
     'dbname': os.environ.get('DB_NAME'),
     'user':  os.environ.get('DB_USER'),
     'password': os.environ.get('DB_PASSWORD'),
-    'host': '127.0.0.1',
+    'host': os.environ.get('DB_HOST'),
     'port': os.environ.get('DB_PORT')
 }
 
